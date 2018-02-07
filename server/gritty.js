066dd3d67b467b95f02c4d50473bf651b0e6bf59
Claude Abounegm <cabounegm@incomestore.com>
2018-02-06 21:06:56 -0500

    Allow env vars to be passed to terminal
    - Just one line change that allows assigning custom environment variables through Socket.io, in addition to env variables passed form the client.

--- server/gritty.js
+++ server/gritty.js
@@ -130,7 +130,7 @@ function onConnection(options, socket) {
     function onTerminal(params) {
         params = params || {};
         
-        const env = params.env;
+        const env = Object.assign({}, params.env, socket.request.env);
         const rows = params.rows;
         const cols = params.cols;
         
