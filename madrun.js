eca337a417984329680717006babcafc1466a03b
coderaiser <coderaiser@cloudcmd.io>
2019-02-21 18:20:10 +0200

    chore(madrun) series -> run

--- madrun.js
+++ madrun.js
@@ -2,19 +2,17 @@
 
 const {
     run,
-    series,
-    parallel,
 } = require('madrun');
 
 module.exports = {
     'test': () => `tape 'test/**/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
-    "lint:test": () => "eslint -c .eslintrc.server test",
-    "lint:server": () => "eslint -c .eslintrc.server --rule 'no-console:0' bin server",
-    "lint:client": () => "eslint --env browser --config .eslintrc client",
-    'lint': () => series(['putout', 'lint:*']),
-    'fix:lint': () => series(['putout', 'lint:*'], '--fix'),
-    'putout': () => `putout bin test client server`,
+    'lint:test': () => 'eslint -c .eslintrc.server test madrun.js',
+    'lint:server': () => 'eslint -c .eslintrc.server --rule \'no-console:0\' bin server',
+    'lint:client': () => 'eslint --env browser --config .eslintrc client',
+    'lint': () => run(['putout', 'lint:*']),
+    'fix:lint': () => run(['putout', 'lint:*'], '--fix'),
+    'putout': () => `putout bin test client server madrun.js`,
     'coverage': () => `nyc ${run('test')}`,
     'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
 };
